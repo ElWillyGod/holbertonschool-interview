0273678e239c41ad6446c89d27897395b9b2f59b
root <root@Willy.lan>
2025-02-04 15:37:54 -0300

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -15,7 +15,7 @@ def canUnlockAll(boxes):
     keys = [0]
     for key in keys:
         for new_key in boxes[key]:
-            if new_key not in keys:
+            if new_key < len(boxes) and new_key not in keys:
                 keys.append(new_key)
 
     if len(keys) == len(boxes):
