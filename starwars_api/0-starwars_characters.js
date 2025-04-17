c5c8c71d792a78e0ec42e6dfddd392509efcca35
ElWillyGod <manuelantognazza@gmail.com>
2025-04-16 22:23:28 -0300

    starwars characters

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+
+request(url, (error, response, body) => {
+	if (error) {
+		console.error(error);
+	}
+
+	const character = JSON.parse(body).characters;
+	const characterNames = character.map(
+		urlname => new Promise((resolve, reject) => {
+			request(urlname, (error, response, body) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(JSON.parse(body).name);
+				}
+			});
+		}));
+
+	Promise.all(characterNames).then(names => console.log(names.join('\n'))
+	).catch(error => console.error(error)
+	);
+
+});
