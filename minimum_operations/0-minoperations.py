1aec040cf355948acef59fe9f1274ac3e2aa72fc
ElWillyGod <manuelantognazza@gmail.com>
2025-04-21 17:28:07 -0300

    recursed

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Minimum Operations
+
+"""
+
+def minOperations(n):
+    """
+    Function that calculates the fewest number of operations needed
+    """
+
+    if n <= 1:
+        return 0
+
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return i + minOperations(n // i)
