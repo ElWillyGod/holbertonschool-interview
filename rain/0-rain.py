745439eb49388ed9509e2170e12c192a92332c4e
WillyGod <122815227+ElWillyGod@users.noreply.github.com>
2025-04-27 13:27:55 -0300

    Update 0-rain.py

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# pylint: skip-file
+
 """
 0-rain
 """
 
-def rain(walls):
 
+def rain(walls):
     """
     Args:
         walls (list): list of non-negative integers representing the height of walls
@@ -18,15 +18,14 @@ def rain(walls):
     total = 0
 
     for i in range(1, a - 1):
-        
         left = walls[i]
 
         for j in range(i):
-            left = max (left, walls[j])
+            left = max(left, walls[j])
         right = walls[i]
 
         for k in range(i + 1, a):
-            right = max (right, walls[k])
+            right = max(right, walls[k])
 
         total += min(left, right) - walls[i]
 
