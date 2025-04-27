0b28cf40a8425701d12a4166fbd03786ba180b21
ElWillyGod <manuelantognazza@gmail.com>
2025-04-27 13:09:17 -0300

    rain 1

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,2 +1,32 @@
 #!/usr/bin/python3
+"""
+0-rain
+"""
 
+def rain(walls):
+
+    """
+    Args:
+        walls (list): list of non-negative integers representing the height of walls
+
+    Returns:
+        int: the amount of rainwater trapped
+    """
+
+    a = len(walls)
+    total = 0
+
+    for i in range(1, a - 1):
+        
+        left = walls[i]
+
+        for j in range(i):
+            left = max (left, walls[j])
+        right = walls[i]
+
+        for k in range(i + 1, a):
+            right = max (right, walls[k])
+
+        total += min(left, right) - walls[i]
+
+    return total
